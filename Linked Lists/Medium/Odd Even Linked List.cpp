7c865357906b50abe2faea89c18e10912dca8711
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-01-21 09:41:45 +0530

    Update Odd Even Linked List.cpp

--- Linked Lists/Medium/Odd Even Linked List.cpp
+++ Linked Lists/Medium/Odd Even Linked List.cpp
@@ -54,3 +54,36 @@ public:
         return ho;
     }
 };
+
+//APPROACH 2: Without extra space
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+           return head;
+        }
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* even_s=head->next;
+        while(odd->next && even->next){
+            odd->next=even->next;
+            even->next=odd->next->next;
+            odd=odd->next;
+            even=even->next;
+        }
+        odd->next=even_s;
+        return head;
+    }
+};
