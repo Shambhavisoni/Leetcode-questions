6e1512aaf49c6172d538b91cf95211532c8a4fb7
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-01-25 07:17:36 +0530

    Create Find the duplicate number.cpp

---
+++ Arrays/Medium/Find the duplicate number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()){
+            if(nums[i]!=i+1 && nums[i]!=nums[nums[i]-1]){
+                swap(nums[i],nums[nums[i]-1]);
+            }
+            else{
+                i++;
+            }
+        }
+        return nums[nums.size()-1];
+    }
+};
